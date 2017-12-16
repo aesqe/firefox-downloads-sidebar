9732da780743474a07f1a235df8b48a853511dbd
aesqe <aesqe@skyphe.org>
2017-12-16 20:46:37 +0100

    v0.2: use Ractive, improve functionality, add svg icons

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ This extension will displays a list of your latest downloads in a sidebar.
 
 Open your Firefox sidebar and choose "Downloads" from the top drop-down list.
 
-Be warned: this was made in an afternoon and as such it is nowhere near complete. I do intend to work further on it.
+Be warned: this was made in two afternoons and as such it is nowhere near complete.
+I do intend to work further on it.
+
+Credits:
+
+* uses Ractive (https://github.com/ractivejs/ractive)
+* folder, play and pause icons by ionicons (http://ionicons.com)
+* downloads icon by Danilo Demarco (http://www.danilodemarco.com)
+* retry icon by Artyom Khamitov (https://www.iconfinder.com/Kh.Artyom)
+* cancel icon by Anastasya Bolshakova (https://www.iconfinder.com/nastu_bol)
--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,185 +1,346 @@
-const contentEl = document.querySelector("#content");
-const { IN_PROGRESS, INTERRUPTED, COMPLETE } = browser.downloads.State;
+Ractive.DEBUG = false;
+Ractive.defaults.allowExpressions = false;
 
-function itemTemplate(item) {
-  const filename = getFilename(item.filename);
-  const parsedUrl = new URL(item.url);
-  const hostname = parsedUrl.hostname || parsedUrl.href;
-  const size = getFileSizeString(item.totalBytes);
-  const date = new Date(item.startTime);
-  const localeDate = date.toLocaleDateString();
-  const localeTime = date.toLocaleTimeString();
+let app;
+const DOWNLOAD_PROGRESS_INTERVAL = 500;
 
-  return `
-    <img class="item-icon" src="" />
-    <div class="item-info overflow-ellipsis">
-      <div class="item-filename overflow-ellipsis" title="${item.filename}">${filename}</div>
-      <span class="item-size">${size}</span>
-      &mdash;
-      <span class="item-domain" title="${item.url}">${hostname}</span>
-      &mdash;
-      <span class="item-datetime">${localeDate} ${localeTime}</span>
-      <div class="progress">
-        <span class="loader"></span>
-        <span class="percentage"></span>
-      </div>
-    </div>
-    <a class="item-show" title="Show file in folder: ${item.filename}"></a>
-  `;
-}
+fetchFileContentsAsText("template-item.html")
+  .then((text) => Ractive.partials.item = text)
+  .then(start);
 
-function getFileSizeString(size) {
-  const measure = [
-    "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
-  ];
-  let counter = 0;
+function start() {
+  app = new Ractive({
+    target: "#content",
+    template: `{{#items:i}}
+      {{> item}}
+    {{/items}}`,
 
-  while (size >= 1024) {
-    size /= 1024;
-    counter++;
-  }
+    states: browser.downloads.State,
+    interrupts: browser.downloads.InterruptReason,
 
-  size = size.toFixed(1);
+    data() {
+      return {
+        items: [],
+      };
+    },
 
-  return `${size} ${measure[counter]}`;
-}
+    onconfig() {
+      this.checkDownloadState = this.checkDownloadState.bind(this);
+      this.prepareItems = this.prepareItems.bind(this);
+      this.prepareItem = this.prepareItem.bind(this);
+      this.setFileIcon = this.setFileIcon.bind(this);
+      this.addItem = this.addItem.bind(this);
+      this.onChanged = this.onChanged.bind(this);
+      this.onErased = this.onErased.bind(this);
 
-function getFilename(path) {
-  return path.replace(/\\+/g, "/").split("/").pop();
-}
+      browser.downloads.onCreated.addListener(this.addItem);
+      browser.downloads.onChanged.addListener(this.onChanged);
+      browser.downloads.onErased.addListener(this.onErased);
 
-function updateIconUrl(itemId, iconUrl) {
-  const downloadIcon = document.querySelector(`#item-${itemId} .item-icon`);
+      this.on({
+        selectItem: this.selectItem,
+        openItem: this.openItem,
+        showItem: this.showItem,
+        changeDownloadState: this.changeDownloadState,
+        cancelDownload: this.cancelDownload,
+      });
 
-  if (downloadIcon) {
-    downloadIcon.setAttribute("src", iconUrl);
-  }
-}
+      this.updateItems();
+    },
 
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
+    updateItems() {
+      this.getLatestDownloads()
+        .then(this.prepareItems)
+        .then((items) => {
+          this.set("items", items);
+          items.forEach(this.setFileIcon);
+          items.forEach(this.checkDownloadState);
+        });
+    },
 
-function getLatestDownloads(limit = 10, orderBy = ["-startTime"]) {
-  return browser.downloads.search({
-    orderBy,
-    limit,
-  });
-}
+    prepareItem(item) {
+      const filePath = item.filename;
+      const fileName = this.getFilename(filePath);
+      const parsedUrl = new URL(item.url);
+      const hostname = parsedUrl.hostname || parsedUrl.href;
+      const size = this.getFileSizeString(item.bytesReceived);
+      const date = new Date(item.startTime);
+      const localeDate = date.toLocaleDateString();
+      const localeTime = date.toLocaleTimeString();
+      const dateTime = `${localeDate} ${localeTime}`;
+      const pauseText = item.state === this.states.INTERRUPTED ? "Resume" : "Pause";
+      const percentage = item.bytesReceived / item.totalBytes;
 
-function updateContent(item = null) {
-  getLatestDownloads()
-    .then(processDownloads, onError);
-}
+      return Object.assign({}, item, {
+        size,
+        filePath,
+        fileName,
+        hostname,
+        dateTime,
+        pauseText,
+        percentage,
+      });
+    },
 
-function getDownloadById(itemId) {
-  return browser.downloads.search({id: itemId});
-}
+    prepareItems(items) {
+      return items.map(this.prepareItem);
+    },
 
-function trackItemDownloadProgress(itemId) {
-  const itemEl = document.querySelector(`#item-${itemId}`);
-  const progressEl = document.querySelector(`#item-${itemId} .progress`);
+    getItemById(itemId) {
+      const items = this.get("items");
+      return items.find((item) => item.id === itemId);
+    },
 
-  itemEl.classList.add("in_progress");
+    getItemIndexById(itemId) {
+      const items = this.get("items");
+      return items.findIndex((item) => item.id === itemId);
+    },
 
-  const intervalId = setInterval(function() {
-    getDownloadById(itemId).then((items) => {
-      const item = items.pop();
-      const downloadComplete = (item.bytesReceived === item.totalBytes);
+    setFileIcon(item) {
+      const itemIndex = this.getItemIndexById(item.id);
+      const keypath = `items.${itemIndex}.iconUrl`;
 
-      updateItemDownloadProgress(item, progressEl);
+      return browser.downloads.getFileIcon(item.id)
+        .then((iconUrl) => this.set(keypath, iconUrl));
+    },
 
-      if (downloadComplete) {
-        clearInterval(intervalId);
-        itemEl.classList.remove("in_progress");
+    search(query){
+      return browser.downloads.search(query);
+    },
+
+    getLatestDownloads(limit = 100) {
+      return this.search({
+        orderBy: ["-startTime"],
+        limit,
+      });
+    },
+
+    getDownloadById(itemId) {
+      return this.search({id: itemId});
+    },
+
+    getFileSizeString(size) {
+      const measure = [
+        "bytes", "KB", "MB", "GB", "TB",
+        "PB", "EB", "ZB", "YB"
+      ];
+      let counter = 0;
+
+      while (size >= 1024) {
+        size /= 1024;
+        counter++;
       }
-    })
-  }, 1000);
-}
 
-function updateItemDownloadProgress(item, progressEl) {
-  const ratio = item.bytesReceived / item.totalBytes;
-  const percentage = (ratio * 100).toFixed(1);
+      size = size.toFixed(1);
 
-  progressEl.querySelector(".loader").style = `width: ${percentage}%`;
-  progressEl.querySelector(".percentage").innerText = `${percentage}%`;
-}
+      return `${size} ${measure[counter]}`;
+    },
 
-function processDownloads(items) {
-  contentEl.innerHTML = "";
+    getFilename(path) {
+      return path.replace(/\\+/g, "/").split("/").pop();
+    },
 
-  items.forEach((item) => {
-    const itemId = item.id;
-    const itemEl = document.createElement("div");
+    deselectAllItems() {
+      const items = this.get("items");
 
-    itemEl.id = `item-${itemId}`;
-    itemEl.className = "item";
-    itemEl.innerHTML = itemTemplate(item);
-    itemEl.title = "Double-click to open the file directly";
+      items.forEach((item) => {
+        const itemIndex = this.getItemIndexById(item.id);
+        const keypath = `items.${itemIndex}.selected`;
 
-    const showItemEl = itemEl.querySelector(".item-show");
+        if (this.get(keypath)) {
+          this.set(keypath, false);
+        }
+      });
+    },
 
-    itemEl.addEventListener("click", selectItem);
-    itemEl.addEventListener("dblclick", openItem);
-    showItemEl.addEventListener("click", showItem);
+    selectItem(event) {
+      const item = event.get();
+      const itemIndex = this.getItemIndexById(item.id);
 
-    contentEl.appendChild(itemEl);
+      this.deselectAllItems();
+      this.set(`items.${itemIndex}.selected`, true);
+    },
 
-    if (item.state === IN_PROGRESS) {
-      trackItemDownloadProgress(item.id);
-    } else if (item.state === INTERRUPTED) {
-      itemEl.classList.add("interrupted");
-    } else {
-      itemEl.classList.add("complete");
+    openItem(event) {
+      const item = event.get();
+      browser.downloads.open(item.id);
+    },
+
+    showItem(event) {
+      const item = event.get();
+      browser.downloads.show(item.id);
+    },
+
+    changeDownloadState(event) {
+      const item = event.get();
+
+      switch (item.downloadState) {
+        case "in_progress":
+          return this.pauseDownload(item.id);
+        case "paused":
+          return this.resumeDownload(item.id);
+        default:
+          return this.retryDownload(item);
+      }
+    },
+
+    pauseDownload(itemId) {
+      return browser.downloads.pause(itemId)
+        .then(null, this.onError);
+    },
+
+    resumeDownload(itemId) {
+      return browser.downloads.resume(itemId)
+        .then(null, this.onError);
+    },
+
+    retryDownload(item) {
+      browser.downloads.erase({id: item.id})
+          .then(this.download(item), this.onError);
+    },
+
+    download(item) {
+      return browser.downloads.download({
+        url: item.url,
+        filename: item.fileName,
+      });
+    },
+
+    cancelDownload(event) {
+      const item = event.get();
+
+      if (item.downloadState === "canceled") {
+        return browser.downloads.erase({id: item.id})
+          .then(null, this.onError);
+      }
+
+      return browser.downloads.cancel(item.id)
+        .then(nu, this.onError);
+    },
+
+    addItem(data) {
+      const item = this.prepareItem(data);
+
+      this.unshift('items', item);
+
+      this.setFileIcon(item);
+      this.checkDownloadState({id: item.id});
+    },
+
+    onChanged(data) {
+      this.checkDownloadState({id: data.id});
+    },
+
+    onErased(itemId) {
+      const itemIndex = this.getItemIndexById(itemId);
+      this.splice("items", itemIndex, 1);
+    },
+
+    onError(error) {
+      console.log("downloads error:", error);
+    },
+
+    calculateDownloadState(item) {
+      const complete = (item.state === this.states.COMPLETE);
+      const in_progress = (item.state === this.states.IN_PROGRESS);
+      const canceled = (item.error === this.interrupts.USER_CANCELED);
+      const resumable = item.paused && item.canResume;
+      const errored = !!this.interrupts[item.error];
+
+      if (errored) {
+        if (canceled) {
+          if (resumable) {
+            return "paused";
+          }
+
+          return "canceled";
+        }
+
+        return "failed";
+      }
+
+      if (complete) {
+        return "complete";
+      }
+      
+      return "in_progress";
+    },
+
+    getStateButtonText(state) {
+      switch (state) {
+        case "paused":
+          return "Resume";
+        case "canceled":
+        case "failed":
+          return "Retry?";
+        default:
+          return "Pause";
+      }
+    },
+
+    checkDownloadState(item) {
+      this.downloadChecker(item.id);
+    },
+
+    downloadChecker(itemId) {
+      const self = this;
+
+      this.search({id: itemId})
+        .then((items) => {
+          const item = items.pop();
+          const downloadState = self.updateDownloadItem(item);
+
+          if (downloadState === "in_progress") {
+            setTimeout(
+              self.downloadChecker(item.id),
+              DOWNLOAD_PROGRESS_INTERVAL
+            );
+          }
+        });
+    },
+
+    updateDownloadItem(item) {
+      const itemIndex = this.getItemIndexById(item.id);
+      const keypath = `items.${itemIndex}`;
+      const oldItem = this.get(keypath);
+      const downloadState = this.calculateDownloadState(item);
+      const stateButtonText = this.getStateButtonText(downloadState);
+      const cancelButtonText = downloadState === "canceled" ? "Erase" : "Cancel";
+      const ratio = item.bytesReceived / item.totalBytes;
+      const downloadInProgress = (
+        downloadState === "paused" || downloadState === "in_progress"
+      );
+
+      let size = oldItem.size;
+      let percentage = oldItem.percentage;
+
+      if (item.bytesReceived > 0) {
+        size = this.getFileSizeString(item.bytesReceived);
+        percentage = (ratio * 100).toFixed(1)
+      }
+
+      this.set(`${keypath}.downloadInProgress`, downloadInProgress);
+      this.set(`${keypath}.cancelButtonText`, cancelButtonText);
+      this.set(`${keypath}.stateButtonText`, stateButtonText);
+      this.set(`${keypath}.downloadState`, downloadState);
+      this.set(`${keypath}.percentage`, percentage);
+      this.set(`${keypath}.size`, size);
+
+      return downloadState;
     }
-
-    return browser.downloads.getFileIcon(itemId)
-      .then((iconUrl) => updateIconUrl(itemId, iconUrl));
   });
 }
 
-function selectItem(event) {
-  const el = getEventItem(event);
-  const selected = document.querySelector(".selected");
+function fetchFileContentsAsText(filePath) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
 
-  if (selected) {
-    selected.classList.remove("selected");
-  }
+    reader.addEventListener("loadend", (e) => {
+      resolve(e.target.result);
+    });
 
-  el.classList.add("selected");
-}
-
-function openItem(event) {
-  event.preventDefault();
-  browser.downloads.open(getEventItemId(event));
-}
-
-function showItem(event) {
-  event.preventDefault();
-  browser.downloads.show(getEventItemId(event));
-}
-
-function getEventItem(event) {
-  let el = event.target;
-
-  while (!el.classList.contains("item")) {
-    el = el.parentElement;
-  }
-
-  return el;
-}
-
-function getEventItemId(event) {
-  const el = getEventItem(event);
-
-  return parseInt(el.id.replace("item-", ""), 10);
-}
-
-browser.windows.getCurrent({populate: true})
-  .then((windowInfo) => {
-    updateContent();
+    fetch(filePath)
+      .then((response) => response.blob())
+      .then((blob) => reader.readAsText(blob));
   });
-
-browser.downloads.onCreated.addListener(updateContent);
-browser.downloads.onChanged.addListener(updateContent);
-browser.downloads.onErased.addListener(updateContent);
+}
